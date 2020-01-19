8c379880b94321e699052de20a1860e973fe408d
DorkmasterFlek <dorkmasterflek@gmail.com>
2020-01-19 12:55:46 -0500

    Updated for new pack format, and new release version

--- scripts/init.lua
+++ scripts/init.lua
@@ -3,6 +3,12 @@ ScriptHost:LoadScript("scripts/settings.lua")
 
 ScriptHost:LoadScript("scripts/logic.lua")
 
+Tracker:AddItems("items/items.json")
+Tracker:AddMaps("maps/maps.json")
+Tracker:AddLocations("locations/locations.json")
+Tracker:AddLayouts("layouts/tracker.json")
+Tracker:AddLayouts("layouts/broadcast.json")
+
 if _VERSION == "Lua 5.3" then
     ScriptHost:LoadScript("scripts/autotracking.lua")
 else
