bc2b81a3811622be67326720db8a54e67d7e8bda
DorkmasterFlek <dorkmasterflek@gmail.com>
2020-02-23 14:41:28 -0500

    Begin logic refactoring for v11

--- scripts/init.lua
+++ scripts/init.lua
@@ -2,16 +2,27 @@
 ScriptHost:LoadScript("scripts/constants.lua")
 ScriptHost:LoadScript("scripts/settings.lua")
 
-ScriptHost:LoadScript("scripts/logic.lua")
+-- TODO: Access logic scripts
+ScriptHost:LoadScript("scripts/logic/common.lua")
+ScriptHost:LoadScript("scripts/logic/alttp/dungeons.lua")
+ScriptHost:LoadScript("scripts/logic/sm/wreckedship.lua")
 
+-- Items
 Tracker:AddItems("items/items.json")
+
+-- Maps
 Tracker:AddMaps("maps/maps.json")
-Tracker:AddLocations("locations/locations.json")
+
+-- TODO: Locations
+Tracker:AddLocations("locations/sm/wreckedship.json")
+
+-- Layouts
 Tracker:AddLayouts("layouts/tracker.json")
 Tracker:AddLayouts("layouts/broadcast.json")
 
+-- Autotracking if supported
 if _VERSION == "Lua 5.3" then
     ScriptHost:LoadScript("scripts/autotracking.lua")
 else
-    print("Auto-tracker is unsupported by your tracker version")
+    print("Autotracking is unsupported by your EmoTracker version, please update to the latest version!")
 end
